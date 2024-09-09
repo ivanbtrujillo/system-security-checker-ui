7c3bfc00cad4b9b28b0277c1ba055de30f3de0a1
Miguel Ordoñez <miordev@gmail.com>
2024-09-09 17:49:31 +0100

    (chore) change the report title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import {useEffect, useState} from "react";
-import {supabase} from "@/lib/supabase";
-import {SecurityReport} from "@/types/security-report";
-import {SecurityReportTable} from "@/components/security-report-table";
-import {SecurityStatsChart} from "@/components/security-report-chart";
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
+import { SecurityReport } from "@/types/security-report";
+import { SecurityReportTable } from "@/components/security-report-table";
+import { SecurityStatsChart } from "@/components/security-report-chart";
 
 export default function Home() {
   const [reports, setReports] = useState<SecurityReport[]>([]);
 
   useEffect(() => {
     async function fetchReports() {
-      const {data, error} = await supabase.from("security_reports").select("*");
+      const { data, error } = await supabase
+        .from("security_reports")
+        .select("*");
       if (error) console.error("Error fetching reports:", error);
       else setReports(data || []);
     }
@@ -20,9 +22,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen h-screen flex-col items-center justify-between p-8 bg-zinc-800 text-white">
-      <h1 className="text-4xl font-bold mb-4 ">
-        iVANTA 🤖 Security Checker Dashboard
-      </h1>
+      <h1 className="text-4xl font-bold mb-4 ">Security Checker Dashboard</h1>
 
       <SecurityStatsChart reports={reports} />
       <SecurityReportTable reports={reports} />
